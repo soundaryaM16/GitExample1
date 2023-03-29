b4f6fcf247ae8e97bdef1106093374a22454326f
soundaryaM16 <soundaryariya08@gmail.com>
2023-03-29 13:22:32 +0530

    mathi done

--- src/org/demo/GitExample1.java
+++ src/org/demo/GitExample1.java
@@ -2,8 +2,19 @@ package org.demo;
 
 public class GitExample1 {
 	
-	public static void main(String[] args) {
-		int a=10;
-		System.out.println(a);
+	public void add(int a,int b) {
+		
+		int c=a+b;
+		System.out.println("sum :"+c);
 	}
+	
+	public static void main(String[] args) {
+		
+		GitExample1 gt=new GitExample1();
+		gt.add(56, 7);
+		
+	}
+		 
+		
+	
 }
